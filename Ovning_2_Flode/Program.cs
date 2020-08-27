427b77ed19e13ed86b978fdba604f974aeca2f4a
stelstel <sthlm_32@hotmail.com>
2020-08-27 14:56:30 +0200

    Add project files.

---
+++ Ovning_2_Flode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ovning_2_Flode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Huvudmenyn");
+            Console.Write("Välj alternativ: ");
+        }
+    }
+}
+
+/*
+ Berätta för användaren att de har kommit till huvudmenyn och de kommernavigera genom att skriva in siffror för att testa olika funktioner.
+ 
+ 2.Skapa skalet till en Switch-sats som till en början har Två Cases. Ett för ”0” somstänger ner programmet och ett default som berättar att det är felaktig input.
+ 
+ 3.Skapa en ​oändlig iteration, ​alltså något som inte tar slut innan vi säger till att denska ta slut. 
+ Detta löser ni med att skapa en egen bool med tillhörande while-loop.4.Bygg ut menyn med val att exekvera de övriga övningarna.
+ */
