60cc2e43f45ec1d07ab04c17cf8a29115a6cb511
stelstel <sthlm_32@hotmail.com>
2020-08-27 15:39:57 +0200

    Menu

--- Ovning_2_Flode/Program.cs
+++ Ovning_2_Flode/Program.cs
@@ -1,22 +1,70 @@
 ﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
 
 namespace Ovning_2_Flode
 {
     class Program
     {
+        private static bool terminate = false;
+
         static void Main(string[] args)
+        {   
+            do
+            {
+                printMenu();
+                handleMenu();
+            } while (terminate == false);
+        }
+
+
+        private static void printMenu()
         {
+            //Console.Clear();
             Console.WriteLine("Huvudmenyn");
+            Console.WriteLine("1: Ungdom eller pensionär");
+            Console.WriteLine("2: Upprepa tio gånger");
+            Console.WriteLine("3: Det tredje ordet");
+            Console.WriteLine("0: Avsluta");
             Console.Write("Välj alternativ: ");
+        }
+
+
+        private static int handleMenu()
+        {
+            int choice;
+            
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                choice = -1;
+            }
+
+            switch (choice)
+            {
+                case 0:
+                    terminate = true;
+                    break;
+                case 1:
+                    // ungdom pensionär
+                    break;
+                case 2:
+                    // Upprepa tio gånger
+                    break;
+                case 3:
+                    // Det tredje ordet
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Felaktig inmatning. Försök igen!");
+                    break;
+            }
+
+            return choice;
         }
     }
 }
-
-/*
- Berätta för användaren att de har kommit till huvudmenyn och de kommernavigera genom att skriva in siffror för att testa olika funktioner.
- 
- 2.Skapa skalet till en Switch-sats som till en början har Två Cases. Ett för ”0” somstänger ner programmet och ett default som berättar att det är felaktig input.
- 
- 3.Skapa en ​oändlig iteration, ​alltså något som inte tar slut innan vi säger till att denska ta slut. 
- Detta löser ni med att skapa en egen bool med tillhörande while-loop.4.Bygg ut menyn med val att exekvera de övriga övningarna.
- */
